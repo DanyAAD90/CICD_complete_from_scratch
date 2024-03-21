80387798cdfaee091482f4d6a25c96ba221375e2
DanyAAD90 <117837948+DanyAAD90@users.noreply.github.com>
2024-03-21 23:53:03 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+#----plik znajduje sie w folderze /wordpress ----
+#!/bin/bash
+
+set -x
+
+sudo apt-get update -y
+sudo apt-get install wget curl vim -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -y
+
+sudo apt-cache policy docker-ce
+
+sudo apt-get install docker-ce -y
+
+#systemctl status docker
+
+sudo usermod -aG docker ubuntu
+
+groups
+
+#---- docker compose ----
+sudo apt-get install docker-compose -y
+
+touch wpadminport8123
